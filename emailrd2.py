d80f3bcd33545bfda412a8276465fd003a55db5a
Nisarg Vinchhi <nvinchh@ncsu.edu>
2016-08-11 14:34:23 -0400

    Cleaning up

--- emailrd2.py
+++ emailrd2.py
@@ -2,41 +2,10 @@ import getpass, imaplib
 from flanker import mime
 import email as em2
 import json
-from textblob import TextBlob as tb
 import nltk
 import re
 import base64
 from bs4 import BeautifulSoup
-
-
-# Function to get the word count
-def tf(word, blob):
-    if words.has_key(word):
-        wCount = words.get(word)
-        wCount = wCount + blob.words.count(word)
-        words[word] = wCount
-    else:
-        words[word] = blob.words.count(word)
-    return words
-
-# Connecting to IMAP Server and logging in
-M = imaplib.IMAP4_SSL("imap.gmail.com", 993)
-email = raw_input("Email address: ")
-password = getpass.getpass()
-M.login(email, password)
-
-
-M.select()
-
-
-# Searching/Fetching the emails from the inbox
-typ, data = M.search(None, 'SUBJECT', "Welcome!")
-#typ, data = M.fetch(data[0], '(RFC822)')
-#emailbody = data[0][1]
-#msg2 = em2.message_from_string(emailbody)
-#mailString = str(data[0][1])
-#listOfEmails = [[]]
-
 
 def clean_html(html):
     # First we remove inline JavaScript/CSS:
@@ -51,6 +20,17 @@ def clean_html(html):
     cleaned = re.sub(r"  ", " ", cleaned)
     cleaned = re.sub(r"  ", " ", cleaned)
     return cleaned.strip()
+
+# Connecting to IMAP Server and logging in
+M = imaplib.IMAP4_SSL("imap.gmail.com", 993)
+email = raw_input("Email address: ")
+password = getpass.getpass()
+M.login(email, password)
+M.select()
+
+# Searching/Fetching the emails from the inbox
+typ, data = M.search(None, 'SUBJECT', "Welcome!")
+
 # Storing the information from the email. From, To, Date, CC, Body and Word Count in the body
 emailDict = {}
 with open('./eData2.json', 'w') as f:
@@ -70,16 +50,6 @@ with open('./eData2.json', 'w') as f:
                 #print raw
         else:
             raw = BeautifulSoup(msg2.get_payload(decode=True), 'html.parser').get_text()
-            #print raw
-        #print type(msg.parts)
-        #if type(msg.parts) == list:
-        #    continue
-        #else:
-        #a = msg.parts[1].body
-            #if a is not None:
-            #print type(a)
-            #raw = clean_html(msg.parts[1].body)
-            #raw = BeautifulSoup(msg.parts[1].body, 'html.parser').get_text().encode('ascii', 'replace')
         tokens = nltk.word_tokenize(raw)
         posTag = nltk.pos_tag(tokens)
         for word in posTag:
@@ -89,15 +59,6 @@ with open('./eData2.json', 'w') as f:
         emailInfo['date'] = msg2['Date']
         emailInfo['to'] = msg2['To']
         emailInfo['cc'] = msg2['Cc']
-                #emailInfo['body'] = msg.parts[0].body
-        #a.encode('ascii', 'replace')
-        #tBlob = tb(a)
-        #words = {}
-        #for word in tBlob.words:
-         #   tf(word, tBlob)
-        #emailInfo['wCount'] = words
-            #else:
-                #continue
         emailDict[n] = emailInfo
         print '-----------------------------------------------------------------------------'
 
@@ -105,15 +66,6 @@ with open('./eData2.json', 'w') as f:
 
 print len(emailDict)
 f.close()
-#for part in msg.parts:
- #   print 'Content-Type: {}'.format(part), part.body
-
-
-#msg = mime.from_string(mailString)
-#for part in msg2.walk():
-#    print part.get_content_type()
-#print msg.headers.items()
-
 
 M.close()
 M.logout()
